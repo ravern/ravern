2bd88c670805a109db81a4a532128ce83a034c75
Ravern Koh <ravernkoh@gmail.com>
2024-10-06 15:13:18 +0800

    remove static site generator, simplify site

--- src/constants.ts
+++
@@ -1,5 +0,0 @@
-export const SERVE_PORT = 8080;
-
-export const OUTPUT_DIR = "docs";
-
-export const STATIC_FILE_DIRS = ["css"];
--- src/context.ts
+++
@@ -1,5 +0,0 @@
-import { Logger } from "https://deno.land/std@0.220.1/log/mod.ts";
-
-export interface Context {
-  logger: Logger;
-}
